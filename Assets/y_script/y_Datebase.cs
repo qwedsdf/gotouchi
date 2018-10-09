3115c0c042a53ebb524a3c7d74c35cd91f773f84
qwedsdf <yohayohayohayohayoha@gmaill.com>
2018-10-09 15:00:08 +0900

    test
    test

--- Assets/y_script/y_Datebase.cs
+++ Assets/y_script/y_Datebase.cs
@@ -1,22 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
 public class y_Datebase : MonoBehaviour {
 
+    //キャラのデータ
 	struct date {
-		//
-		string place_name;
+        Sprite img;//画像
+		string place_name;//場所
+        string description;//説明文章
 	}
+
+    List<date> DateBase = new List<date>();
 
 	// Use this for initialization
 	void Start () {
-        int a;
+        //string path = "/hoge/hogehoge/hoge.png";
+        //string fileName = System.IO.Path.GetDirectoryName(path);
+        //Debug.Log(fileName);
 
+       // Sprite i = Resources.Load<Sprite>("DateChar/hukuoka/ふなっしー/img");
+        //Debug.Log(i.name);
+        string str = Application.dataPath + "/Resources/DateChar";
+        string[] files = System.IO.Directory.GetFiles(@str, "*", System.IO.SearchOption.AllDirectories);
+        for (int i = 0; i < files.Length; i++) {
+            if (System.IO.Path.GetExtension(files[i]) == ".meta") continue;
+            Debug.Log(files[i]);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+       
 	}
 }
