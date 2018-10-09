18dfd14c245386efe3537694e9672c8b1dc44e65
qwedsdf <yohayohayohayohayoha@gmaill.com>
2018-10-09 11:59:24 +0900

    データベース作成途中
    スクリプト作っただけ

---
+++ Assets/y_script/y_Datebase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class y_Datebase : MonoBehaviour {
+
+	struct date {
+		//
+		string place_name;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
